7f2eb6351ba21541dd6eb86506633493310d080f
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2023-03-11 19:35:41 +0700

    remove menu admin

--- components/elements/MenuUserProfile.tsx
+++ components/elements/MenuUserProfile.tsx
@@ -56,13 +56,7 @@ export default function MenuUserProfile({}: Props) {
     router.push(Routes.userListFeeds);
     setLoading(true);
   }
-  async function onAdmin() {
-    if (Routes.admin === router.pathname) {
-      return;
-    }
-    router.push(Routes.admin);
-    setLoading(true);
-  }
+
   return (
     <>
       <div className="button-menu" onClick={onSettingUser}>
@@ -81,12 +75,7 @@ export default function MenuUserProfile({}: Props) {
         <MessageOutlined />
         <a>Liên hệ với chúng tôi</a>
       </div>
-      {isRole === "ADMIN" ? (
-        <div className="button-menu" onClick={onAdmin}>
-          <CodeSandboxOutlined />
-          <a>Quản trị viên</a>
-        </div>
-      ) : null}
+
       <div className="button-logout d-flex" onClick={onLogOut}>
         <LogoutOutlined />
         <a>Logout</a>
